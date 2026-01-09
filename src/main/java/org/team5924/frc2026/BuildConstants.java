feaddf591b1484efdea0eb950b39c5e66f95cfa5
James Guleno <jguleno27@siprep.org>
2026-01-08 16:55:58 -0800

    fixed copyrights in util files

--- src/main/java/org/team5924/frc2026/BuildConstants.java
+++ src/main/java/org/team5924/frc2026/BuildConstants.java
@@ -21,12 +21,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "GoldenGateRobotics2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "60adc2028a0a5dc01d7f2d332e1223d399e4c545";
-  public static final String GIT_DATE = "2026-01-03 16:10:14 EST";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "8e95c43a9e33bce5f439bcfae54de567f2d0d7c2";
+  public static final String GIT_DATE = "2026-01-08 17:00:27 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-08 16:14:32 EST";
-  public static final long BUILD_UNIX_TIME = 1767906872217L;
+  public static final String BUILD_DATE = "2026-01-08 19:55:21 EST";
+  public static final long BUILD_UNIX_TIME = 1767920121082L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
