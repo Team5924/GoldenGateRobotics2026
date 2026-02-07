7afbb3526f253c11460295a27fc2a5ffe287564d
James Guleno <jguleno27@siprep.org>
2026-02-07 15:30:28 -0800

    start work on sensors yipe

---
+++ src/main/java/org/team5924/frc2026/subsystems/beamBreak/BeamBreakIO.java
@@ -0,0 +1,14 @@
+package org.team5924.frc2026.subsystems.beamBreak;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public class BeamBreakIO {
+//   @AutoLog
+//   class CoralSensorIOInputs {
+//     public CoralSensorIOData data = new CoralSensorIOData(0.0, false);
+//   }
+
+//   record CoralSensorIOData(boolean broken) {}
+
+//   default void updateInputs(CoralSensorIOInputs inputs) {}    
+}
